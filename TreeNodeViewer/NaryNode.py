8b0f9a4f1c5d991813a57a822178c03f0398260f
Manabu Kiyota <agmk4655@gmail.com>
2021-11-23 17:27:17 +0900

    Create NaryNode.py
    1st versin of NaryNode.py

---
+++ TreeNodeViewer/NaryNode.py
@@ -0,0 +1,47 @@
+class NaryNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, node):
+        self.children.append(node)
+
+    def __str__(self):
+        return "{}: ".format(self.value) + " ".join([str(x.value) for x in self.children])
+
+
+def main():
+    Root = NaryNode("Root")
+    A = NaryNode("A")
+    B = NaryNode("B")
+    C = NaryNode("C")
+    D = NaryNode("D")
+    E = NaryNode("E")
+    F = NaryNode("F")
+    G = NaryNode("G")
+    H = NaryNode("H")
+    I = NaryNode("I")
+    Root.add_child(A)
+    Root.add_child(B)
+    Root.add_child(C)
+    A.add_child(D)
+    A.add_child(E)
+    C.add_child(F)
+    D.add_child(G)
+    F.add_child(H)
+    F.add_child(I)
+
+    print(Root)
+    print(A)
+    print(B)
+    print(C)
+    print(D)
+    print(E)
+    print(F)
+    print(G)
+    print(H)
+    print(I)
+
+
+if __name__ == "__main__":
+    main()
